dacc1dc779507a0a209509f46ad2b906397d6de9
kbtapjm <kbtapjm@gmail.com>
2019-04-01 13:39:18 +0900

    first commit

---
+++ src/test/java/kr/co/pjm/diving/enjoydivingmq/EnjoyDivingMqApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.co.pjm.diving.enjoydivingmq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EnjoyDivingMqApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
